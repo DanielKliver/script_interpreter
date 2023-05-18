bdbd5170c7d52b64ad9b6f6b47efb89b175dcf7e
DanielKliver <povishevd@mail.ru>
2023-05-18 18:58:44 +0700

    almost ready

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#define SIZE_OF_STRING 100
+
+
+enum 
+{
+	SIZE_OF_STRING = 100
+};
 
 int errors(int number_of_error)
 {
@@ -14,6 +19,10 @@ int errors(int number_of_error)
 	{
 		case 1:
 			printf("File opening error!\n");
+			return -1;
+			break;
+		case 2:
+			printf("Error condition of whle!\n");
 			return -1;
 			break;
 		default:
@@ -37,20 +46,24 @@ void strip(char *s)
 	*p2 = '\0';
 }
 
-int parser_of_read(char *mas_for_read, int count, FILE *prog)
+int parser_of_read(char *mas_for_read, int count, FILE *prog, int ret)
 {
 	int coincidence = 0;
 	char read[] = "read";
+	int in_place_count = count;//вплёл переменную для считывания сравнений
 	for(int i = 0; i<4;i++)
 	{
-		if(read[i] == mas_for_read[i])
+		if(read[i] == mas_for_read[in_place_count])//вот тут
 		{
 			coincidence++;
 		}
+		in_place_count++;//вот тож. На этом её полномочия окончены
+
 	}
 
 	if(coincidence == 4)
 	{
+		ret++;
 		fputs("double ", prog);
 		count += 4;
 		while(mas_for_read[count] == ' ')
@@ -71,22 +84,174 @@ int parser_of_read(char *mas_for_read, int count, FILE *prog)
 		fputs(";\nscanf(\"%f\", ", prog);
 		fputs(name, prog);
 		fputs(");\n", prog);
-
-
+		free(name);
 	}
 	return 0;
 
 }
 
+int parser_start_while(char *mas_for_read, int count, FILE* prog, int ret)
+{
+	int coincidence = 0;
+	int in_place_count = count;
+	char read[] = "while";
+	for(int i = 0; i<5;i++)
+	{
+		if(read[i] == mas_for_read[in_place_count])
+		{
+			coincidence++;
+		}
+		in_place_count++;
+	}
+	if(coincidence == 5)
+	{
+		ret++;
+		fputs("while(", prog);
+		count +=5;
+
+
+		while(mas_for_read[count]==' ')
+		{
+			count++;
+		}
+
+		char *condition = malloc(SIZE_OF_STRING);
+		int count_condition = 0;
+		while((mas_for_read[count]!='d' && mas_for_read[count+1]!='o'))
+		{
+			condition[count_condition] = mas_for_read[count];
+			count_condition++;
+			count++;
+			if(count >= SIZE_OF_STRING)
+			{
+				errors(2);
+			}
+		}
+		strip(condition);
+		fputs(condition, prog);
+		fputs(")\n", prog);
+		fputs("{\n", prog);
+		free(condition);
+	}
+	return 0;	
+
+
+}
+int parser_end_while(char *mas_for_read, int count, FILE* prog, int ret)
+{
+	int coincidence = 0;
+	int in_place_count = count;
+	char read[] = "done";
+	for(int i = 0; i<4;i++)
+	{
+		if(read[i] == mas_for_read[in_place_count])
+		{
+			coincidence++;
+		}
+		in_place_count++;
+	}
+	if(coincidence == 4)
+	{
+		ret++;
+		fputs("}\n", prog);
+		count +=4;
+	}
+	return 0;
+}
+
+int parser_start_if(char *mas_for_read, int count, FILE* prog, int ret)
+{
+	int coincidence = 0;
+	int in_place_count = count;
+	char read[] = "if";
+	for(int i = 0; i<2;i++)
+	{
+		if(read[i] == mas_for_read[in_place_count])
+		{
+			coincidence++;
+		}
+		in_place_count++;
+	}
+	if(coincidence == 2)
+	{
+		ret++;
+		fputs("if(", prog);
+		count +=3;
+		while(mas_for_read[count]==' ')
+		{
+			count++;
+		}
+
+		char *condition = malloc(SIZE_OF_STRING);
+		int count_condition = 0;
+		while((mas_for_read[count]!='t' && mas_for_read[count+1]!='h' && mas_for_read[count+2]!='e' && mas_for_read[count+3]!='n'))
+		{
+			condition[count_condition] = mas_for_read[count];
+			count_condition++;
+			count++;
+			if(count >= SIZE_OF_STRING)
+			{
+				errors(2);
+			}
+		}
+		strip(condition);
+		fputs(condition, prog);
+		fputs(")\n", prog);
+		fputs("{\n", prog);
+		free(condition);
+	}
+
+return 0;
+}
+
+int parser_end_if(char *mas_for_read, int count, FILE* prog, int ret)
+{
+	int coincidence = 0;
+	int in_place_count = count;
+	char read[] = "fi";
+	for(int i = 0; i<2;i++)
+	{
+		if(read[i] == mas_for_read[in_place_count])
+		{
+			coincidence++;
+		}
+		in_place_count++;
+	}
+	if(coincidence == 2)
+	{
+		ret++;
+		fputs("}\n", prog);
+		count +=2;
+	}
+	return 0;
+}
+
+void parser_of_operation(char *mas_for_read, int count, FILE* prog, int ret)
+{
+	if(ret<1)
+	{
+		fputs(mas_for_read, prog);
+	}
+}
+
 void parser(char *mas_for_read, FILE *prog)
 {
+
 	int count = 0;
+
 	while(mas_for_read[count] == ' ')
 	{
 		count++;
 	}
 
-	parser_of_read(mas_for_read, count, prog);
+	int ret = 0;
+	parser_of_read(mas_for_read, count, prog, ret);
+	parser_start_while(mas_for_read, count, prog, ret);
+	parser_end_while(mas_for_read, count, prog, ret);
+	parser_start_if(mas_for_read, count, prog, ret);
+	parser_end_if(mas_for_read, count, prog, ret);
+	parser_of_operation(mas_for_read, count, prog, ret);
+	
 }
 
 void work_with_files()
@@ -96,17 +261,18 @@ void work_with_files()
 
 	if(!script||!prog)
 	{
-		printf("File opening error!\n");
 		errors(1);
 	}
 
 	fputs("#include<stdio.h>\nint main()\n{\n", prog);
 
-	char *mas_for_read = malloc(SIZE_OF_STRING*sizeof(char));
-	fgets(mas_for_read, SIZE_OF_STRING, script);
-
-	parser(mas_for_read, prog);
-
+	while(feof(script)==0)
+	{
+		char *mas_for_read = malloc(SIZE_OF_STRING);
+		fgets(mas_for_read, SIZE_OF_STRING, script);
+		parser(mas_for_read, prog);
+		free(mas_for_read);
+	}
 
 	fclose(script);
 	fclose(prog);
--- src/script_prog.c
+++ src/script_prog.c
@@ -3,3 +3,24 @@ int main()
 {
 double n;
 scanf("%f", n);
+read n
+double min;
+scanf("%f", min);
+read min
+i=0
+while(i < n )
+{
+while i < n do
+double x;
+scanf("%f", x);
+ read x
+if(min > x )
+{
+ if min > x then
+ min = x
+}
+ fi
+ i = i + 1
+}
+done
+ръмя
